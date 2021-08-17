45f727cd99d2941d6ea066514af1b44552ec485f
Sourav Naga <sourav.naga@kreeti.com>
2021-08-17 10:34:27 +0530

    change model view controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.new(name: params[:name],
+       email: params[:email], 
+       password: params[:password], 
+       password_confirmation: params[:password_confirmation])
+
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "User Successfully Created!"
+      redirect_to root_path
+    else
+      flash[:warning] = @user.errors.full_messages.join(", ")
+      redirect_to root_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :home, only: [:show]
+  resources :sessions, only: [:new, :create, :destroy]
+  get "signup", to: "users#new", as: "signup"
+  post "/users" => "users#create"
+  get "login", to: "sessions#new", as: "login"
+
+  get "logout", to: "sessions#destroy", as: "logout"
+
+  root "home#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
